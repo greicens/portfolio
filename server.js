15124b3095f8ca93253a83970d54c599402e89c2
greicens <gsilva@mail.ccsf.edu>
2017-02-18 15:21:53 -0800

    initial files

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {root: __dirname});
+  });
+
+  app.listen(process.env.PORT || 3000, function () {
+    console.log('Example app listening at http://localhost:3000/');
+  });
